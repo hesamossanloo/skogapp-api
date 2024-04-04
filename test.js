0ecf45d4bdfa16e738df923c7252b5119b355ec3
Hesam Ossanloo (Personal) <hesam@ossanloo.com>
2024-04-04 17:47:00 +0100

    Update timeout and memory size, add new dependencies, and remove unused files

--- test.js
+++
@@ -1,6 +0,0 @@
-fetch('https://toblyuimg8.execute-api.eu-north-1.amazonaws.com/dev/wms')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch((error) => {
-    console.error('Error:', error);
-  });
